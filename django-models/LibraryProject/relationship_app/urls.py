be977ceb7c669edad9aa2bca17fa0c36f55be67f
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-07-20 23:22:21 +0100

    updates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    
+    # Optional: listing view if you have one
+    path('books/', views.book_list, name='book_list'),
 ]
