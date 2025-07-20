f161506b299c94ee39342dc1acf629fe3560a8ab
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-07-20 20:24:34 +0100

    function based view implementation

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+
+    if not books:
+        return HttpResponse("No books found.")
+
+    output = "Books in Database:\n"
+    for book in books:
+        output += f"- {book.title} by {book.author.name}\n"
+
+    return HttpResponse(output, content_type="text/plain")
