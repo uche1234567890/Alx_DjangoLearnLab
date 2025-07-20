c21d34457a9feff9f90d05e979b668a07641aafb
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-07-20 20:53:25 +0100

    url config

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -20,7 +20,9 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path('', include('relationship_app.urls')),
 ]
 
