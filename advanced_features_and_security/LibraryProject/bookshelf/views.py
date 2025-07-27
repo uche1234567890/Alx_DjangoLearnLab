d3531913496672f085489f5f96e331e9fff8a103
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-07-27 19:04:11 +0100

    views update

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
+from .forms import ExampleForm
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def list_books(request):
